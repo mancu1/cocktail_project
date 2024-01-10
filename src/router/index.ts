e055c1bb87f62d10b4f8596f0b55f4ec2e48e011
Mansur Zakirov <zakirovmi1@gmail.com>
2024-01-10 19:53:58 +0300

    change router type

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import CocktailView from '@/views/CocktailView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
@@ -23,7 +23,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
